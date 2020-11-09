72f1508b772c5a597b34be54dac4f291d3c8c14d
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2020-11-09 23:09:32 +0200

    hvi

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-var a = [1, 3, 6];
-a = a.concat(4, 5);
-document.write(a);
+var a;
+var b = 326;
+var c = b;
+var d = null;
+document.write("<p>a = " + a);
+document.write("<p>b = " + b);
+document.write("<p>c = " + c);
+document.write("<p>d = " + d);
