1adc4b06b62696fbc0d221e390f2fca672449383
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2020-11-10 00:29:03 +0200

    cutf

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-var a;
-var b = 326;
-var c = b;
-var d = null;
-document.write("<p>a = " + a);
-document.write("<p>b = " + b);
-document.write("<p>c = " + c);
-document.write("<p>d = " + d);
+function f() {
+    var myVar = "Hello World";
+}
+
+document.write(myVar);
