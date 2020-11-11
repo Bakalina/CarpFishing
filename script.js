d510cb5c2c2181f544428f2e91568c7d335cc888
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2020-11-11 19:57:11 +0200

    fyvv

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 ﻿var x = 6;
 var y = 15;
-var x = 4;
+var z = 4;
 
-document.write(fjm
+document.write(x += y - x++ * z);
+document.write("<hr/>");
+document.write(z = --x - y * 5);
+document.write("<hr/>");
+document.write(y /= x + 5 % z);
+document.write("<hr/>");
+document.write(z = x++ + y * 5);
+document.write("<hr/>");
+document.write(x = y - x++ * z);
+document.write("<hr/>");
